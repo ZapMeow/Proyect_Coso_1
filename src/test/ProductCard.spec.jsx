2209576144ed915c34e1e8e61e035daec33e8c2c
CattoElectrico <lucas8422olmedoj@gmail.com>
2025-12-12 11:06:18 -0300

    a2

--- src/test/ProductCard.spec.jsx
+++ src/test/ProductCard.spec.jsx
@@ -1,38 +1,39 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import ProductCard from '../components/ProductCard'
 import React from 'react'
 
 describe('ProductCard', () => {
-  const mockProps = {
-    code: "1",
-    title: 'Producto de prueba',
-    distributor: 'Distribuidor Ejemplo',
-    distributorLink: 'https://distribuidor.com',
-    price: 123.45,
-    description: 'Esta es una descripción de prueba.',
-    category: 'categoria1',
-    rating: 8,
-    image: 'https://via.placeholder.com/150'
+  const mockProduct = {
+    idProduct: "1",
+    nameProduct: 'Producto de prueba',
+    distributorProduct: 'Distribuidor Ejemplo',
+    linkDistributor: 'https://distribuidor.com',
+    priceProduct: 123.45,
+    categoryProduct: 'categoria1',
+    urlImage: 'https://via.placeholder.com/150'
   }
 
   it('Renderiza correctamente las propiedades', () => {
-    render(<ProductCard {...mockProps} />)
+    render(
+      <MemoryRouter>
+        <ProductCard product={mockProduct} />
+      </MemoryRouter>
+    )
 
-    expect(screen.getByText(mockProps.title)).toBeInTheDocument()
+    expect(screen.getByText(mockProduct.nameProduct)).toBeInTheDocument()
 
-    const distributorLink = screen.getByRole('link', { name: mockProps.distributor })
-    expect(distributorLink).toHaveAttribute('href', mockProps.distributorLink)
+    const distributorLink = screen.getByRole('link', { name: mockProduct.distributorProduct })
+    expect(distributorLink).toHaveAttribute('href', mockProduct.linkDistributor)
 
-    expect(screen.getByText(`$${mockProps.price}`)).toBeInTheDocument()
-    expect(screen.getByText(`${mockProps.rating}/10 ⭐`)).toBeInTheDocument()
-    expect(screen.getByText(mockProps.description)).toBeInTheDocument()
+    expect(screen.getByText(`$${mockProduct.priceProduct}`)).toBeInTheDocument()
+    expect(screen.getByText('0/10 ⭐')).toBeInTheDocument()
 
-    const image = screen.getByAltText(mockProps.title)
-    expect(image).toHaveAttribute('src', mockProps.image)
+    const image = screen.getByAltText(mockProduct.nameProduct)
+    expect(image).toHaveAttribute('src', mockProduct.urlImage)
 
     expect(screen.getByText('Agregar')).toBeInTheDocument()
-    expect(screen.getByText('Calificar')).toBeInTheDocument()
 
-    expect(screen.getByRole('img').parentElement).toHaveClass(`card ${mockProps.category} current-card`)
+    expect(screen.getByRole('img').parentElement).toHaveClass(`card text-white p-2 m-2 ${mockProduct.categoryProduct} current-card transition`)
   })
 })
